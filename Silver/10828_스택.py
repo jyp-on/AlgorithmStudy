2c3292fea1322bdc77994359518f27d3206fcf09
mythpoy <okmlnsunok@gmail.com>
2022-06-19 18:40:41 +0900

    10828 완료

--- Silver/10828_스택.py
+++ Silver/10828_스택.py
@@ -1,24 +1,30 @@
+import sys
+input = sys.stdin.readline
+
 stack = []
 command_length = int(input())
-while command_length!=0:
+
+for _ in range(command_length):
     ord = input()
+    ord = ord[:-1] #readline으로 받으면 끝에 개행문자가 포함되어서 제거해줘야함.
 
-    if 'push' == ord[0:4]:
-        stack.push(ord[5])
-    if 'top' == ord:
-
-
-
-
-
-
-def size():
-    return len(stack)
-def empty():
-    if size() == 0:
-        return 1
-    else:
-        return 0
-def top():
-    return stack[0]
-
+    if ord.startswith('push'):
+        _, num = ord.split()
+        stack.append(num)
+    elif 'pop' == ord:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif 'size' == ord:
+        print(len(stack))
+    elif 'empty' == ord:
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    elif 'top' == ord:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
