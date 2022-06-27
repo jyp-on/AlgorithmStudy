7ae5b9ba24574ba068b59b3f08b39fc0ceca5bcc
mythpoy <okmlnsunok@gmail.com>
2022-06-27 16:20:18 +0900

    사분면

---
+++ BOJ/14681_사분면 고르기.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+result = 0
+if (x > 0) & (y > 0):
+    result = 1
+elif (x < 0) & (y > 0):
+    result = 2
+elif (x < 0) & (y < 0):
+    result = 3
+elif (x > 0) & (y < 0):
+    result = 4
+print(result)
