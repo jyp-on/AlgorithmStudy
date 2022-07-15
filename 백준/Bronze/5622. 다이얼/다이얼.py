ae1c8031d81cf42264fdeaf1eef9713a67838121
박주영 <52206904+mythpoy@users.noreply.github.com>
2022-07-15 14:41:05 +0900

    [Bronze II] Title: 다이얼, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,20 @@
+s = input()
+time = []
+for i in range(len(s)):
+    if s[i] in 'ABC':
+        time.append(3)
+    elif s[i] in 'DEF':
+        time.append(4)
+    elif s[i] in 'GHI':
+        time.append(5)
+    elif s[i] in 'JKL':
+        time.append(6)
+    elif s[i] in 'MNO':
+        time.append(7)
+    elif s[i] in 'PQRS':
+        time.append(8)
+    elif s[i] in 'TUV':
+        time.append(9)
+    elif s[i] in 'WXYZ':
+        time.append(10)
+print(sum(time))
