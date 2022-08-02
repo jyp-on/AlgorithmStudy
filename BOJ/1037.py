25393832fc1a3d2f638f3fc0a60e5374a13451b4
mythpoy <okmlnsunok@gmail.com>
2022-08-02 21:56:26 +0900

    .

---
+++ BOJ/1037.py
@@ -0,0 +1,3 @@
+N_len = int(input())
+약수 = list(map(int, input().split()))
+print(min(약수)*max(약수))
---
+++ BOJ/2981 검문.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = []
+for _ in range(int(input())):
+    N.append(int(input()))
+
+def find(i):
+    temp = set()
+    for k in N:
+        temp.add(k%i)
+        if len(temp)>1:
+            return
+    if len(temp)==1:
+        print(i, end= ' ')
+
+
+for i in range(2, int(min(N)+1)):
+    find(i)
+
